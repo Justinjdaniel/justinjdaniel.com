beae55ae40b68c3ee14181178cc45f9f4229e920
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-07-01 13:16:28 +0530

    config(prod): configuration updated

--- next.config.js
+++ next.config.js
@@ -1,17 +1,93 @@
-/**
- * @type {import('next').NextConfig}
- */
+const { get } = require("@vercel/edge-config");
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
+	// output: "export",
 	// Optional: Add a trailing slash to all paths `/about` -> `/about/`
-	trailingSlash: true,
+	// trailingSlash: true,
 	// Optional: Change the output directory `out` -> `dist`
-	distDir: "out",
+	// distDir: "out",
+	experimental: {
+		serverActions: true,
+	},
 	// Optional: Disable the image optimizer.
 	images: {
-		unoptimized: true,
+		// 	unoptimized: true,
 		formats: ["image/avif", "image/webp"],
+		// Twitter Profile Picture
+		// remotePatterns: [
+		// 	{
+		// 		protocol: "https",
+		// 		hostname: "pbs.twimg.com",
+		// 		pathname: "/**",
+		// 	},
+		// ],
+	},
+	redirects() {
+		try {
+			return get("redirects");
+		} catch {
+			return [];
+		}
+	},
+	headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: securityHeaders,
+			},
+		];
 	},
 };
 
-module.exports = nextConfig;
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+    default-src 'self' vercel.live;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self' data:;
+`;
+
+const securityHeaders = [
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\n/g, ""),
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+	{
+		key: "Referrer-Policy",
+		value: "origin-when-cross-origin",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+	{
+		key: "X-Frame-Options",
+		value: "DENY",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=31536000; includeSubDomains; preload",
+	},
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=()",
+	},
+];
+
+module.exports = withContentlayer(nextConfig);
