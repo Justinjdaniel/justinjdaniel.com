f6aac2ef417236edc187d8551dec0d135c18ea2a
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-08-16 16:09:06 +0530

    feat: :lipstick: new mirco-interaction added for nav hover
    a navigatiion hover effect added

--- app/page.js
+++ app/page.js
@@ -44,9 +44,20 @@ const Header = () => {
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-purple-500 hover:scale-110"
+							className="text-sm duration-500 text-zinc-500 hover:text-purple-500
+							link relative overflow-hidden px-0 py-[7px]"
 						>
 							{item.name}
+							<svg
+								className="link__graphic link__graphic--slide"
+								width="300%"
+								height="100%"
+								viewBox="0 0 1200 60"
+								preserveAspectRatio="none"
+							>
+								<title>wave</title>
+								<path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0" />
+							</svg>
 						</Link>
 					))}
 				</ul>
@@ -108,8 +119,7 @@ const ProfileContent = () => {
 			<h1 className="font-bold text-2xl mb-8 text-left animate-fade-in w-max">
 				hey, I'm{" "}
 				<span
-					className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500
-				hover:from-violet-500 hover:to-cyan-500 duration-500"
+					className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
 				>
 					Justin{" "}
 				</span>
