d629e5570cec6b448f0e2d253368998e06b82ff1
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2024-06-16 02:15:51 +0530

    feat: Refactor profile content images rendering in ProfileContent component

--- app/page.js
+++ app/page.js
@@ -6,24 +6,37 @@ import teamMeetUpEnd2023 from "public/images/home/team-meetup-end-2023.webp";
 import teamMeetUpMid2023 from "public/images/home/team-meetup-mid-2023.webp";
 import React, { Fragment } from "react";
 
-/**
- * Renders the profile content section of the webpage.
- *
- * @return {JSX.Element} The profile content section.
- */
 const ProfileContent = () => {
-	// TODO: Section for Projects
-	// Add a section for content regarding projects, achievements, and tools I've worked with. Also, consider including links to another page that provides detailed information about the projects I've completed, those currently in progress, as well as any side projects or personal endeavors.
-	// assignees: justinjdaniel
-	// labels: enhancement, fix-me, ui
+	const images = [
+		{
+			alt: "Speaking in blockchain meetup",
+			title: "Speaking in blockchain meetup",
+			src: speaking,
+		},
+		{
+			alt: "Awarded by Kerala Digital University's Vice Chancellor",
+			title: "Awarded by Kerala Digital University's Vice Chancellor",
+			src: award,
+		},
+		{
+			alt: "Team meetup in Kerala mid 2023",
+			title: "Team meetup in Kerala mid 2023",
+			src: teamMeetUpMid2023,
+		},
+		{
+			alt: "Team meetup in Kerala mid 2023",
+			title: "Team meetup in Kerala mid 2023",
+			src: teamMeetUpEnd2023,
+		},
+	];
 
 	return (
 		<content className="z-10 antialiased max-w-2xl mb-40 mx-4 mt-8 md:mx-auto">
 			<h1 className="font-semibold text-2xl mb-8 text-left w-max">
-				hey, I'm{" "}
+				Hey, I'm{" "}
 				<span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-					Justin{" "}
-				</span>
+					Justin
+				</span>{" "}
 				👋
 			</h1>
 			<div>
@@ -34,63 +47,31 @@ const ProfileContent = () => {
 					background and DevOps experience, I ensure seamless integration and
 					deployment for any blockchain project. My passion for blockchain
 					technology extends to ongoing research to stay updated with the latest
-					advancements in the field. conduct research to stay updated with
-					emerging technologies.
+					advancements in the field.
 				</p>
 			</div>
-			<div className="columns-2 gap-4 my-8">
-				<div className="relative h-80 mb-4">
-					<Image
-						alt="speaking in blockchain meetup"
-						title="speaking in blockchain meetup"
-						src={speaking}
-						fill="true"
-						sizes="(max-width: 768px) 213px, 50vw"
-						priority="true"
-						className="rounded-lg object-cover h-80 w-full transition-transform duration-300 ease-in-out hover:scale-105"
-					/>
-				</div>
-				<div className="relative h-40">
-					<Image
-						alt="Team meetup in Kerala mid 2023"
-						title="Team meetup in Kerala mid 2023"
-						src={teamMeetUpMid2023}
-						fill="true"
-						sizes="(max-width: 768px) 213px, 50vw"
-						priority="true"
-						className="rounded-lg object-cover h-40 w-full transition-transform duration-300 ease-in-out hover:scale-105"
-					/>
-				</div>
-
-				<div className="relative h-40 mb-4">
-					<Image
-						alt="Awarded by Kerala Digital University's Vice Chancellor"
-						title="Awarded by Kerala Digital University's Vice Chancellor"
-						src={award}
-						sizes="(max-width: 768px) 213px, 50vw"
-						priority="true"
-						className="rounded-lg object-cover h-40 w-ful transition-transform duration-300 ease-in-out hover:scale-105"
-					/>
-				</div>
-				<div className="relative h-80">
-					<Image
-						alt="Team meetup in Kerala mid 2023"
-						title="Team meetup in Kerala mid 2023"
-						src={teamMeetUpEnd2023}
-						sizes="(max-width: 768px) 213px, 50vw"
-						priority="true"
-						className="rounded-lg object-cover h-80 w-full transition-transform duration-300 ease-in-out hover:scale-105"
-					/>
-				</div>
+			<div className="grid grid-cols-2 gap-4 my-8">
+				{images.map(({ alt, title, src }, index) => (
+					<div key={index + alt} className="relative h-60">
+						<Image
+							alt={alt}
+							title={title}
+							src={src}
+							fill="true"
+							sizes="(max-width: 768px) 213px, 50vw"
+							priority="true"
+							className="rounded-lg object-cover h-full w-full transition-transform duration-300 ease-in-out hover:scale-105 sm:object-center"
+						/>
+					</div>
+				))}
 			</div>
 			<div className="mb-4 text-zinc-300">
-				<p className="prose prose-neutral dark:prose-invert mb-4 text-pretty">
+				<p className="prose prose-neutral dark:prose-invert mb-4">
 					I lead the development and quality assurance of secure blockchain
 					solutions. My expertise spans public and private platforms like
 					Ethereum, Polygon, and Hyperledger Fabric.
 				</p>
-
-				<p className="prose prose-neutral dark:prose-invert text-pretty">
+				<p className="prose prose-neutral dark:prose-invert">
 					I spearheaded the creation of a user-friendly, multi-platform DApp NFT
 					platform for organizations and institutes. Additionally, I led the
 					development of a private blockchain service for efficient supply chain
@@ -100,9 +81,8 @@ const ProfileContent = () => {
 					system to enhance supply chain transparency.
 				</p>
 			</div>
-
 			<div>
-				<p className="prose prose-neutral dark:prose-invert text-pretty">
+				<p className="prose prose-neutral dark:prose-invert">
 					I'm a lifelong learner, constantly seeking new blockchain challenges
 					and skills. I thrive in collaborative environments, working with
 					developers and visionaries to build a more decentralized and secure
