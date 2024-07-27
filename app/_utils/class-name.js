d5cf46fe38e74fb01c5d7932566744e86d19dfe6
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2024-07-27 17:27:53 +0530

    ✨ Add utility functions for handling color palettes and merging class names

---
+++ app/_utils/class-name.js
@@ -0,0 +1,13 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Utility function to merge tailwind classes and regular class names.
+ * It uses the `clsx` and `twMerge` functions from the `clsx` and `tailwind-merge` libraries.
+ *
+ * @param {...string} inputs - The class names to merge.
+ * @return {string} The merged class names.
+ */
+export function cn(...inputs) {
+	return twMerge(clsx(inputs));
+}
